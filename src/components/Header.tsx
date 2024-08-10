eeabe5e9d139cffbdce2c41b41790a5fdc93c00c
Justin Headley <headley.justin@gmail.com>
2024-08-10 13:20:03 -0700

    chore: Update logo size in Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,10 +3,10 @@ import { Box, Heading, Flex, Image } from '@chakra-ui/react';
 
 const Header: React.FC = () => {
     return (
-        <Box as="header" bg="gray.100" py={4}>
+        <Box as="header" bg="gray.100" py={0}>
             <Flex maxW="container.xl" mx="auto" alignItems="center">
                 <Heading as="h1" size="xl">
-                    <Image src={'/src/assets/logo.png'} alt={'awesearch logo'} boxSize="100px" objectFit="contain" />
+                    <Image src={'/src/assets/logo.png'} alt={'awesearch logo'} boxSize="125px" objectFit="contain" />
                 </Heading>
                 <Heading as="h3" size="md" ml={4} fontWeight="normal">
                     awesomeness curated, development accelerated
