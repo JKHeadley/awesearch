85285ab74fed2b7823c2756821735f18dcda6100
Justin Headley <headley.justin@gmail.com>
2024-08-17 15:47:12 -0700

    feat: Add loading tips to LoadingOverlay component

--- src/components/LoadingOverlay.tsx
+++ src/components/LoadingOverlay.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import {
   Box,
   Text,
@@ -8,25 +8,35 @@ import {
   useBreakpointValue,
 } from '@chakra-ui/react';
 import { motion, AnimatePresence } from 'framer-motion';
-
-const tips = [
-  'AweSearch uses AI to find the perfect tools for your needs.',
-  'Our database includes thousands of developer tools and resources.',
-  'AweSearch can help you discover both open-source and commercial solutions.',
-  'Use specific keywords to get more targeted results.',
-  'AweSearch is constantly updated with the latest developer tools.',
-  'Looking for a specific technology? Include it in your search query!',
-  'AweSearch can help you find alternatives to your current tools.',
-  "Don't forget to check the 'View Details' for in-depth information about each tool.",
-];
+import {
+  mainLoadingTips,
+  additionalLoadingTips,
+} from '../pages/ExampleQueries';
 
 interface LoadingOverlayProps {
   isLoading: boolean;
 }
 
 const LoadingOverlay: React.FC<LoadingOverlayProps> = ({ isLoading }) => {
-  const [currentTip, setCurrentTip] = useState(tips[0]);
+  const [currentTip, setCurrentTip] = useState('');
   const isMobile = useBreakpointValue({ base: true, md: false });
+  const [isMainTip, setIsMainTip] = useState(true);
+
+  const getNextTip = useCallback(() => {
+    if (isMainTip) {
+      const nextTip =
+        additionalLoadingTips[
+          Math.floor(Math.random() * additionalLoadingTips.length)
+        ];
+      setCurrentTip(nextTip);
+      setIsMainTip(false);
+    } else {
+      const nextTip =
+        mainLoadingTips[Math.floor(Math.random() * mainLoadingTips.length)];
+      setCurrentTip(nextTip);
+      setIsMainTip(true);
+    }
+  }, [isMainTip]);
 
   const logoSrc = useColorModeValue(
     '/src/assets/logo-light.png',
@@ -46,15 +56,19 @@ const LoadingOverlay: React.FC<LoadingOverlayProps> = ({ isLoading }) => {
     'rgba(255, 255, 255, 0.1)',
   );
 
+  // Grab the first tip from the main list
+  useEffect(() => {
+    const randomTip =
+      mainLoadingTips[Math.floor(Math.random() * mainLoadingTips.length)];
+    setCurrentTip(randomTip);
+  }, []);
+
   useEffect(() => {
     if (isLoading) {
-      const intervalId = setInterval(() => {
-        setCurrentTip(tips[Math.floor(Math.random() * tips.length)]);
-      }, 5000); // Change tip every 5 seconds
-
+      const intervalId = setInterval(getNextTip, 5000); // Change tip every 5 seconds
       return () => clearInterval(intervalId);
     }
-  }, [isLoading]);
+  }, [isLoading, getNextTip]);
 
   if (!isLoading) return null;
 
