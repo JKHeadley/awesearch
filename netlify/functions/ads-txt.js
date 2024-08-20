8bb5b4f1fea6136ff7c68528b8e8df17d1e88372
Justin Headley <headley.justin@gmail.com>
2024-08-20 10:09:06 -0700

    chore: Add error handling for missing AdSense client ID in ads-txt.js

--- netlify/functions/ads-txt.js
+++ netlify/functions/ads-txt.js
@@ -1,6 +1,19 @@
 exports.handler = async function (event, context) {
+  console.log('ads-txt function triggered');
+
   const publisherId = process.env.VITE_ADSENSE_CLIENT;
+  console.log('Publisher ID:', publisherId);
+
+  if (!publisherId) {
+    console.error('VITE_ADSENSE_CLIENT environment variable is not set');
+    return {
+      statusCode: 500,
+      body: 'Server configuration error',
+    };
+  }
+
   const adsTxtContent = `google.com, ${publisherId}, DIRECT, f08c47fec0942fa0`;
+  console.log('ads.txt content:', adsTxtContent);
 
   return {
     statusCode: 200,
