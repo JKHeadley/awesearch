4abfdf57662e6b5e685fa67852944e3d8482ad7f
Justin Headley <headley.justin@gmail.com>
2024-11-19 18:46:00 -0800

    chore: file

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,1 +1,26 @@
- 
+import { extendTheme, ThemeConfig } from '@chakra-ui/react'
+import { modalTheme } from './modal'
+import { buttonTheme } from './button'
+import { inputTheme } from './input'
+
+const config: ThemeConfig = {
+  initialColorMode: 'light',
+  useSystemColorMode: true,
+  cssVarPrefix: 'awe',
+  disableTransitionOnChange: true,
+}
+
+const theme = extendTheme({
+  config,
+  components: {
+    Modal: modalTheme,
+    Button: buttonTheme,
+    Input: inputTheme,
+    // Disable unused components
+    Drawer: { enabled: false },
+    Popover: { enabled: false },
+    Tooltip: { enabled: false },
+  },
+})
+
+export default theme 
